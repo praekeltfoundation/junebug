b4eec12fe606901817e7c24b9b0ee6208bda0ff3
Rudi Giesler <rudi@praekelt.com>
2015-08-19 15:57:24 +0200

    Revert "Try fix for readthedocs build"
    This reverts commit 5a9b634f6f74829fc13d9929fbcf34ec84a0acf9.

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,6 @@
 
 import sys
 import os
-from mock import Mock as MagicMock
-
-# ReadTheDocs fix, don't import any modules not related to documentation
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return Mock()
-
-MOCK_MODULES = ['klein', 'jsonschema', 'treq']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
