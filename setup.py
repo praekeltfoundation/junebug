e44a747878cadb62e26f99dd156e6203f1dae2c7
Rudi Giesler <rudi@praekelt.com>
2015-09-23 15:20:49 +0200

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name='junebug',
-    version='0.0.4a',
+    version='0.0.4',
     url='http://github.com/praekelt/junebug',
     license='BSD',
     description=(
