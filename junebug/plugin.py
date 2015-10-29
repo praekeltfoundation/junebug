8f193b8ffa084f51dca2bfddc2f224d501572f5d
Rudi Giesler <rudi@praekelt.com>
2015-10-29 12:24:03 +0200

    Change to sphinx style docstrings for base plugin class.

--- junebug/plugin.py
+++ junebug/plugin.py
@@ -2,22 +2,31 @@ class JunebugPlugin(object):
     '''Base class for all Junebug plugins'''
 
     def start_plugin(self, config):
-        '''Can be overridden with any required startup code for the plugin.
+        '''
+        Can be overridden with any required startup code for the plugin.
         Can return a deferred.
 
-        config - Config that Junebug was started with.'''
+        :param config: The config that Junebug was started with.
+        :type config: :class:`JunebugConfig`
+        '''
         pass
 
     def channel_started(self, channel):
-        '''Called whenever a channel is started. Should be implemented by the
+        '''
+        Called whenever a channel is started. Should be implemented by the
         plugin. Can return a deferred.
 
-        channel - The channel that has been started.'''
+        :param channel: The channel that has been started.
+        :type channel: :class:`Channel`
+        '''
         pass
 
     def channel_stopped(self, channel):
-        '''Called whenever a channel is stopped. Should be implemented by the
+        '''
+        Called whenever a channel is stopped. Should be implemented by the
         plugin. Can return a deferred.
 
-        channel - The channel that has been stopped.'''
+        :param channel: The channel that has been stopped.
+        :type channel: :class:`Channel`
+        '''
         pass
