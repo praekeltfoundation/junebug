41d49cde93adab8c33e0eeaf784f2003241b7b1e
Rudi Giesler <rudi@praekelt.com>
2015-11-17 14:35:57 +0200

    Have junebug set `worker_name` for transports

--- junebug/channel.py
+++ junebug/channel.py
@@ -270,6 +270,7 @@ class Channel(object):
         config = self._properties['config']
         config = self._convert_unicode(config)
         config['transport_name'] = self.id
+        config['worker_name'] = self.id
         config['publish_status'] = True
         return config
 
