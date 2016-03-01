9533934ba26ab45f97ce4409ec3f77b4df0d795e
Rudi Giesler <rudi@praekelt.com>
2016-03-01 14:47:12 +0200

    Release 0.1.1

--- junebug/__init__.py
+++ junebug/__init__.py
@@ -5,4 +5,4 @@ interface.
 from junebug.api import JunebugApi
 
 __all__ = ['JunebugApi']
-__version__ = '0.1.1a'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name='junebug',
-    version='0.1.1a',
+    version='0.1.1',
     url='http://github.com/praekelt/junebug',
     license='BSD',
     description=(
