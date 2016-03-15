bf5c0c58a0c86b1a23aa5c28d7cda0ef805d7452
Rudi Giesler <rudi@praekelt.com>
2016-03-15 13:05:28 +0200

    Add environment variable for selecting reactor

--- junebug/__init__.py
+++ junebug/__init__.py
@@ -2,6 +2,28 @@
 Junebug is a system for managing text messaging transports via a RESTful HTTP
 interface.
 '''
+import os
+
+r = os.environ.get('REACTOR')
+if r == "SELECT":
+    from twisted.internet import selectreactor
+    selectreactor.install()
+elif r == "POLL":
+    from twisted.internet import pollreactor
+    pollreactor.install()
+elif r == "KQUEUE":
+    from twisted.internet import kqreactor
+    kqreactor.install()
+elif r == "WFMO":
+    from twisted.internet import win32eventreactor
+    win32eventreactor.install()
+elif r == "IOCP":
+    from twisted.internet import iocpreactor
+    iocpreactor.install()
+elif r == "EPOLL":
+    from twisted.internet import epollreactor
+    epollreactor.install()
+
 from junebug.api import JunebugApi
 
 __all__ = ['JunebugApi']
