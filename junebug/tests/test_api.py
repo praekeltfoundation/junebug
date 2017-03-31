b939916ee883444ac1db228f1ee132e7ddc43a0c
Simon de Haan <simon@praekelt.org>
2017-03-31 16:26:35 +0200

    newer Twisteds have different error output

--- junebug/tests/test_api.py
+++ junebug/tests/test_api.py
@@ -59,7 +59,10 @@ class TestJunebugApi(JunebugTestBase):
             'the URL manually please check your spelling and try again.', {
                 'errors': [{
                     'code': 404,
-                    'message': '404: Not Found',
+                    'message': ('404 Not Found: The requested URL was not '
+                                'found on the server.  If you entered the URL'
+                                ' manually please check your spelling and try'
+                                ' again.'),
                     'type': 'Not Found',
                     }]
                 })
@@ -73,9 +76,9 @@ class TestJunebugApi(JunebugTestBase):
             None, {
                 'errors': [{
                     'code': 301,
-                    'message': '301: Moved Permanently',
-                    'type': 'Moved Permanently',
+                    'message': '301 Moved Permanently: None',
                     'new_url': '%s/channels/' % self.url,
+                    'type': 'Moved Permanently',
                 }],
             })
         yield self.assert_response(
