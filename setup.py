c1a07a10526ff16055edbc009c040d6cb1ef73c3
Erik Harding <erik@praekelt.org>
2017-08-28 12:36:53 +0200

    version bump to 0.1.25

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name='junebug',
-    version='0.1.24',
+    version='0.1.25',
     url='http://github.com/praekelt/junebug',
     license='BSD',
     description=(
