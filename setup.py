940cb4b5d69674a42a008b789a5114e2b7cc649b
Erik Harding <erik@praekelt.org>
2018-07-31 12:37:22 +0200

    fix minimum requirements

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setup(
         'pyasn1',
         'klein<=17.10.0',
         'jsonschema',
-        'treq<16.12.0',
+        'treq<=17.8.0',
         # We install a newer version of twisted before vumi, since vumi has a
         # lower minimum version requirement.
-        'Twisted>=15.3.0',
+        'Twisted>=17.9.0,<18.0.0',
         'vumi>=0.6.18',
         'confmodel',
         'PyYAML',
