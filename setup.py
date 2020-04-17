f8bea9372289e301821c3532f6e2d87319a639e6
Erik Harding <erik@praekelt.org>
2020-04-17 18:05:27 +0200

    release 0.1.43

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name='junebug',
-    version='0.1.42',
+    version='0.1.43',
     url='http://github.com/praekelt/junebug',
     license='BSD',
     description=(
